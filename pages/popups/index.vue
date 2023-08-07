7c62845c8b3942fa4c02f6dd049f7d6a8ec95137
benkissi <ben@benkissi.com>
2023-08-07 20:40:55 +0000

    edit(): add seo tags

--- pages/popups/index.vue
+++ pages/popups/index.vue
@@ -6,6 +6,30 @@ import Pricing from "@/components/sections/Pricing.vue";
 definePageMeta({
   layout: "popups",
 });
+
+useHead({
+  title: "CleverPopups - Realtime social proof that converts!",
+  meta: [
+    {
+      name: "description",
+      content:
+        "Boost your sales with our dynamic and clever purchase popups. Captivate customers, offer exclusive deals, and harness the power of social proof for increased conversions. Elevate your revenue potential today!",
+    },
+    {
+      property: "og:title",
+      content: "CleverPopups - Realtime social proof that converts!",
+    },
+    {
+      property: "og:description",
+      content:
+        "Boost your sales with our dynamic and clever purchase popups. Captivate customers, offer exclusive deals, and harness the power of social proof for increased conversions. Elevate your revenue potential today!",
+    },
+    {
+      property: "og:image",
+      content: "https://cleverapps.studio/og1.png",
+    },
+  ],
+});
 </script>
 
 <template>
