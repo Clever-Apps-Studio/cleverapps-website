abaad1d1ca77cb39223e1d5e076fe6636fbaa2c3
benkissi <ben@benkissi.com>
2025-01-15 10:19:09 +0000

    edit(): add new services

--- components/main/Services.vue
+++ components/main/Services.vue
@@ -9,17 +9,27 @@ import Title from "./Title.vue";
       <div
         class="flex flex-col items-center justify-center p-4 bg-gradient-to-t from-teal-500/50 to-white h-[200px] rounded-2xl cursor-pointer hover:scale-105 transition-all duration-300"
       >
-        <p class="text-xl font-bold text-teal-600">Web Application</p>
+        <p class="text-xl font-bold text-teal-600">Web Application Development</p>
       </div>
       <div
         class="flex flex-col items-center justify-center p-4 bg-gradient-to-t from-indigo-500/50 to-white h-[200px] rounded-2xl cursor-pointer hover:scale-105 transition-all duration-300"
       >
-        <p class="text-xl font-bold text-indigo-600">Mobile Application</p>
+        <p class="text-xl font-bold text-indigo-600">Mobile Application Development</p>
       </div>
        <div
         class="flex flex-col items-center justify-center p-4 bg-gradient-to-t from-sky-500/50 to-white h-[200px] rounded-2xl cursor-pointer hover:scale-105 transition-all duration-300"
       >
-        <p class="text-xl font-bold text-sky-600">Desktop Application</p>
+        <p class="text-xl font-bold text-sky-600">Desktop Application Development</p>
+      </div>
+      <div
+        class="flex flex-col items-center justify-center p-4 bg-gradient-to-t from-red-500/50 to-white h-[200px] rounded-2xl cursor-pointer hover:scale-105 transition-all duration-300"
+      >
+        <p class="text-xl font-bold text-red-600">Website Development</p>
+      </div>
+      <div
+        class="flex flex-col items-center justify-center p-4 bg-gradient-to-t from-lime-500/50 to-white h-[200px] rounded-2xl cursor-pointer hover:scale-105 transition-all duration-300"
+      >
+        <p class="text-xl font-bold text-lime-600">UI/UX Design</p>
       </div>
       <div
         class="flex flex-col items-center justify-center p-4 bg-gradient-to-t from-orange-500/50 to-white h-[200px] rounded-2xl cursor-pointer hover:scale-105 transition-all duration-300"
@@ -27,9 +37,14 @@ import Title from "./Title.vue";
         <p class="text-xl font-bold text-orange-600">Enterprise Software</p>
       </div>
       <div
+        class="flex flex-col items-center justify-center p-4 bg-gradient-to-t from-cyan-500/50 to-white h-[200px] rounded-2xl cursor-pointer hover:scale-105 transition-all duration-300"
+      >
+        <p class="text-xl font-bold text-cyan-600">IT Consultancy</p>
+      </div>
+       <div
         class="flex flex-col items-center justify-center p-4 bg-gradient-to-t from-fuchsia-500/50 to-white h-[200px] rounded-2xl cursor-pointer hover:scale-105 transition-all duration-300"
       >
-        <p class="text-xl font-bold text-fuchsia-600">IT Consultancy</p>
+        <p class="text-xl font-bold text-fuchsia-600">IT Training</p>
       </div>
       <div
         class="flex flex-col items-center justify-center p-4 bg-gradient-to-t from-blue-500/50 to-white h-[200px] rounded-2xl cursor-pointer hover:scale-105 transition-all duration-300"
